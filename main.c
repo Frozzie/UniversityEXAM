d8dc2c34d86a94ba61d8e6ed27351ee6754125e4
Frozzie <yasha.petrukhin@gmail.com>
2024-06-10 11:42:59 +0300

    Finished work

--- main.c
+++ main.c
@@ -1,15 +1,52 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include <time.h>
-#include <stdint.h>
-#include <string.h>
-#include <conio.h>
-#include "matrix.h"
-#include "string_.h"
+#include <errno.h> 
+#include <stdio.h> 
+#include <string.h> 
 
+typedef struct _data1
+{
+    int arr_int[10];
+    int num_int;
+} data1;
 
-int main()
+#define not_in_mem 12
+
+void func1 (data1 *data, int index, int val)
 {
     
+    if (index >= data->num_int)
+    {
+        errno = not_in_mem; // Not in allocated memory
+    }
+    else
+    {
+        data->arr_int[index] = val;
+        errno = 0; // err reset
+    }
+}
+
+int main ()
+{
+    data1 s;
+    int i = 8;
+    s.num_int = 10;
+
+    while(i < 12)
+    {
+        func1 (&s, i++, 21);
+
+        printf("Value of errno: %d\n", errno);
+        perror("Message from perror");
+    }
+    
+    printf("\n");
+
+    while(i > 7)
+    {
+        func1 (&s, i--, 21);
+
+        printf("Value of errno: %d\n", errno);
+        perror("Message from perror");
+    }
+
+    return 0;
 }
